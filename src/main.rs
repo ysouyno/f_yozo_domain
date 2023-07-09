5b95cf4be98dbdd2726bab6dc769f8a33a9b4c9c
ysouyno <ysouyno@163.com>
2023-07-09 09:07:45 +0800

    remove the output of cmd

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
-use std::{io::BufRead, process};
+use std::process;
 
 fn check_ping() -> Result<(), std::io::Error> {
-    let mut child = process::Command::new("cmd")
+    let child = process::Command::new("cmd")
         .args(["/c", "call", "ping", "www.baidu.com"])
         .stdout(process::Stdio::piped())
         .spawn()
-        .expect("Failed to exec ping");
-
-    let stdout = child.stdout.as_mut().ok_or_else(|| {
-        std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Could not capture standard output.",
-        )
-    })?;
-
-    let reader = std::io::BufReader::new(stdout);
-    reader
-        .lines()
-        .map_while(Result::ok)
-        .for_each(|line| println!("{}", line));
+        .expect("Failed to exec ping command.");
 
     let output = child.wait_with_output().unwrap();
     if output.status.success() {
@@ -36,22 +23,18 @@ fn main() {
     let mut count = 1;
 
     loop {
-        match check_ping() {
-            Err(e) => {
-                count += 1;
-                println!("err: {}, Call logon.exe now, times: {}", e, count);
+        if let Err(e) = check_ping() {
+            println!("err: {} Call logon.exe now, times: {}", e, count);
 
-                process::Command::new("cmd")
-                    .args(["/c", "call", "D:\\dnld\\logon.exe"])
-                    .stdout(process::Stdio::piped())
-                    .spawn()
-                    .expect("Failed to exec logon.exe")
-                    .wait_with_output()
-                    .unwrap();
-            }
-            _ => {
-                std::thread::sleep(std::time::Duration::from_secs(3));
-            }
+            process::Command::new("cmd")
+                .args(["/c", "call", "D:\\dnld\\logon.exe"])
+                .stdout(process::Stdio::piped())
+                .spawn()
+                .expect("Failed to exec logon.exe");
+
+            count += 1;
         }
+
+        std::thread::sleep(std::time::Duration::from_secs(3));
     }
 }
